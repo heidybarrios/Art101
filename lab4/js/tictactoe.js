b264792fcae500bfa4211daaf479b08e3bd7baaf
heidybarrios <hbarrios415@gmail.com>
2021-02-18 22:34:56 -0800

    nin
    niniknini

--- lab4/js/tictactoe.js
+++ lab4/js/tictactoe.js
@@ -1,89 +1,126 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
+const statusDiv = document.querySelector('.status');
+const resetDiv = document.querySelector('.reset');
+const cellDivs = document.querySelectorAll('.game-cell');
 
-startGame()
+// game constants
+const xSymbol = '×';
+const oSymbol = '○';
 
-restartButton.addEventListener('click', startGame)
+// game variables
+let gameIsLive = true;
+let xIsNext = true;
 
-function startGame() {
-  circleTurn = false
-  cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(CIRCLE_CLASS)
-    cell.removeEventListener('click', handleClick)
-    cell.addEventListener('click', handleClick, { once: true })
-  })
-  setBoardHoverClass()
-  winningMessageElement.classList.remove('show')
-}
 
-function handleClick(e) {
-  const cell = e.target
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-  placeMark(cell, currentClass)
-  if (checkWin(currentClass)) {
-    endGame(false)
-  } else if (isDraw()) {
-    endGame(true)
+// functions
+const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+
+const handleWin = (letter) => {
+  gameIsLive = false;
+  if (letter === 'x') {
+    statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
   } else {
-    swapTurns()
-    setBoardHoverClass()
+    statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
   }
-}
+};
 
-function endGame(draw) {
-  if (draw) {
-    winningMessageTextElement.innerText = 'Draw!'
+const checkGameStatus = () => {
+  const topLeft = cellDivs[0].classList[1];
+  const topMiddle = cellDivs[1].classList[1];
+  const topRight = cellDivs[2].classList[1];
+  const middleLeft = cellDivs[3].classList[1];
+  const middleMiddle = cellDivs[4].classList[1];
+  const middleRight = cellDivs[5].classList[1];
+  const bottomLeft = cellDivs[6].classList[1];
+  const bottomMiddle = cellDivs[7].classList[1];
+  const bottomRight = cellDivs[8].classList[1];
+
+  // check winner
+  if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[1].classList.add('won');
+    cellDivs[2].classList.add('won');
+  } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+    handleWin(middleLeft);
+    cellDivs[3].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[5].classList.add('won');
+  } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+    handleWin(bottomLeft);
+    cellDivs[6].classList.add('won');
+    cellDivs[7].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[3].classList.add('won');
+    cellDivs[6].classList.add('won');
+  } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+    handleWin(topMiddle);
+    cellDivs[1].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[7].classList.add('won');
+  } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+    handleWin(topRight);
+    cellDivs[2].classList.add('won');
+    cellDivs[5].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+    handleWin(topRight);
+    cellDivs[2].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[6].classList.add('won');
+  } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
+    gameIsLive = false;
+    statusDiv.innerHTML = 'Game is tied!';
   } else {
-    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    xIsNext = !xIsNext;
+    if (xIsNext) {
+      statusDiv.innerHTML = `${xSymbol} is next`;
+    } else {
+      statusDiv.innerHTML = `<span>${oSymbol} is next</span>`;
+    }
   }
-  winningMessageElement.classList.add('show')
-}
+};
 
-function isDraw() {
-  return [...cellElements].every(cell => {
-    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-  })
-}
 
-function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass)
-}
+// event Handlers
+const handleReset = () => {
+  xIsNext = true;
+  statusDiv.innerHTML = `${xSymbol} is next`;
+  for (const cellDiv of cellDivs) {
+    cellDiv.classList.remove('x');
+    cellDiv.classList.remove('o');
+    cellDiv.classList.remove('won');
+  }
+  gameIsLive = true;
+};
 
-function swapTurns() {
-  circleTurn = !circleTurn
-}
+const handleCellClick = (e) => {
+  const classList = e.target.classList;
 
-function setBoardHoverClass() {
-  board.classList.remove(X_CLASS)
-  board.classList.remove(CIRCLE_CLASS)
-  if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS)
+  if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {
+    return;
+  }
+
+  if (xIsNext) {
+    classList.add('x');
+    checkGameStatus();
   } else {
-    board.classList.add(X_CLASS)
+    classList.add('o');
+    checkGameStatus();
   }
-}
+};
 
-function checkWin(currentClass) {
-  return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
-      return cellElements[index].classList.contains(currentClass)
-    })
-  })
+
+// event listeners
+resetDiv.addEventListener('click', handleReset);
+
+for (const cellDiv of cellDivs) {
+  cellDiv.addEventListener('click', handleCellClick)
 }
