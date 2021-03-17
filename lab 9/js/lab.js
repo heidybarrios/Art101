0a0a9dd6cd7ff9cec3556c4d6d855636f0faee5d
heidybarrios <hbarrios415@gmail.com>
2021-03-16 21:55:52 -0700

    Update lab.js
    better changes

--- lab 9/js/lab.js
+++ lab 9/js/lab.js
@@ -9,7 +9,7 @@ var new1El = document.createElement("p");
 new1El.innerHTML = "What is going on here?!";
 new1El.setAttribute("style","color: white"); // makes the p white
 var new2El = document.createElement("h4"); // creates <h3> element
-new2El.innerHTML ="A text tag for h3."; 
+new2El.innerHTML ="A text tag for h3.";
 new2El.setAttribute("style","color: white"); // make h3 white
 outputEl.appendChild(new1El);
 outputEl.appendChild(new2El);
