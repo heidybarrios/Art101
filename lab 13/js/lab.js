dc9844647360948ac2f184c6ef11e2adcd12d28f
heidybarrios <hbarrios415@gmail.com>
2021-03-17 11:39:06 -0700

    labs
    new

---
+++ lab 13/js/lab.js
@@ -0,0 +1,40 @@
+/*
+Author: Heidy Barrios
+Date: March 11, 2021
+Public License
+*/
+
+function fizzBuzz() {
+  var output = "";
+  for(i = 0; i <= 200; i++)
+  {
+    if(i%3 == 0 && i%5 == 0 && i%7 == 0)
+    {
+      output += i + " - FizzBuzzBoom!<br>";
+    }
+    else if(i%3 == 0 && i%5 == 0) {
+      output += i + " - FizzBuzz!<br>";
+    }
+    else if (i%3 == 0 && i%7 == 0) {
+      output += i + " - FizzBoom!<br>";
+    }
+    else if (i%5 == 0 && i%7 == 0) {
+      output += i + " - BuzzBoom!<br>";
+    }
+    else if (i%3 == 0) {
+      output += i + " - Fizz!<br>";
+    }
+    else if (i%5 == 0) {
+      output += i + " - Buzz!<br>";
+    }
+    else if (i%7 == 0) {
+      output += i + " - Boom!<br>";
+    }
+    else
+    {
+      output += i + " -<br>"
+    }
+  }
+  $("#output").html(output);
+}
+fizzBuzz();
